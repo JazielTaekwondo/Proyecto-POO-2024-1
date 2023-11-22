7a7cfc0610c8c0dc5d16baf498b6115298e91ef5
JazielTaekwondo <jazielitomarcial@gmail.com>
2023-11-21 22:59:11 -0600

    Cambios 7
    se ha dividido en tres metodos el fondo el dinosaurio y un metodo que los junta

--- src/doodle/Doodle.java
+++ src/doodle/Doodle.java
@@ -5,6 +5,6 @@ import doodle.jump.equipo7.grafico.Fondo;
 public class Doodle {
     public static void main(String[] args) {
         Fondo fondo = new Fondo();
-        fondo.mostrarFondo();
+        fondo.mostrarFondoyPersonaje();
     }
 }
--- src/doodle/jump/equipo7/grafico/Fondo.java
+++ src/doodle/jump/equipo7/grafico/Fondo.java
@@ -4,32 +4,40 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Fondo extends JFrame {
-
+    JLayeredPane juego = new JLayeredPane();
     public Fondo() {
         super("DOODLE JUMP");
         setLayout(new BorderLayout());
-    }
-
-    public void mostrarFondo() {
+        ImageIcon icon = new ImageIcon(getClass().getResource("/images/Smily.png"));
+        Image iconImage = icon.getImage();
+        setIconImage(iconImage);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 620);
         setLocationRelativeTo(null);
+    }
 
-        JLayeredPane layeredPane = new JLayeredPane();
-        layeredPane.setPreferredSize(new Dimension(400, 620));
+    public void mostrarFondoyPersonaje() {
+        crearFondo();
+        personaje();
+        add(this.juego, BorderLayout.CENTER);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public void crearFondo(){
+        this.juego.setPreferredSize(new Dimension(400, 620));
 
         ImageIcon imagenFondo = new ImageIcon(getClass().getResource("/images/doodleFondo.png"));
         JLabel label1 = new JLabel(imagenFondo);
         label1.setBounds(0, 0, 400, 620);
-        layeredPane.add(label1, Integer.valueOf(0));
+        this.juego.add(label1, Integer.valueOf(0));
 
+    }
+
+    public void personaje(){
         ImageIcon imagenDino = new ImageIcon(getClass().getResource("/images/Smily.png"));
         JLabel label2 = new JLabel(imagenDino);
         label2.setBounds(50, 50, imagenDino.getIconWidth(), imagenDino.getIconHeight());
-        layeredPane.add(label2, Integer.valueOf(1));
-
-        add(layeredPane, BorderLayout.CENTER);
-        setResizable(false);
-        setVisible(true);
+        this.juego.add(label2, Integer.valueOf(1));
     }
 }
