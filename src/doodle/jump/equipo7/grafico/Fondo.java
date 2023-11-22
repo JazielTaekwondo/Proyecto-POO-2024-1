ad6a12d25c15878244f12decb56223c880257a93
JazielTaekwondo <jazielitomarcial@gmail.com>
2023-11-21 21:55:34 -0600

    cambios 6
    muestra dinosaurio y fondo juntos

--- src/doodle/jump/equipo7/grafico/Fondo.java
+++ src/doodle/jump/equipo7/grafico/Fondo.java
@@ -4,21 +4,30 @@ import java.awt.*;
 
 public class Fondo extends JFrame {
     private JFrame fondo;
-    public Fondo(){
+
+    public Fondo() {
         this.fondo = new JFrame("DOODLE JUMP");
-        this.fondo.setLayout(new FlowLayout());
+        this.fondo.setLayout(new BorderLayout());
     }
-    public void mostrarFondo(){
+
+    public void mostrarFondo() {
         this.fondo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.fondo.setSize(400, 620);
+        JLayeredPane layeredPane = new JLayeredPane();
+
+        layeredPane.setPreferredSize(new Dimension(400, 620));
         ImageIcon imagenFondo = new ImageIcon(getClass().getResource("/images/doodleFondo.png"));
         JLabel label1 = new JLabel(imagenFondo);
+        label1.setBounds(0, 0, 400, 620);
+        layeredPane.add(label1, Integer.valueOf(0)); 
+
         ImageIcon imagenDino = new ImageIcon(getClass().getResource("/images/Smily.png"));
         JLabel label2 = new JLabel(imagenDino);
-        this.fondo.add(label1);
-        this.fondo.add(label2);
+        label2.setBounds(50, 50, imagenDino.getIconWidth(), imagenDino.getIconHeight()); 
+        layeredPane.add(label2, Integer.valueOf(1));
+
+        this.fondo.add(layeredPane, BorderLayout.CENTER);
         this.fondo.setResizable(false);
         this.fondo.setVisible(true);
-        this.fondo.setLocationRelativeTo(null);
     }
 }
