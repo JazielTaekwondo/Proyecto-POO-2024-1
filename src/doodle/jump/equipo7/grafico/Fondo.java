cbef32885e6c4cd08c4ee45159a1506b61976edd
Rodrigo <rodrigo.lugofi@gmail.com>
2023-11-21 23:55:56 -0600

    Movimientos derecha e izquierda
    Agregue un KeyListener para capturar eventos de teclado y mover el dinosaurio hacia la izquierda o derecha según la tecla presionada en la compu. También cambia la imagen png del dinosaurio para que mire hacia la izquierda o derecha, según la dirección del movimiento.
    
    Para que los eventos de teclado se activen, el JFrame debe tener el foco. Por eso, se añadido setFocusable(true) en el constructor.

--- src/doodle/jump/equipo7/grafico/Fondo.java
+++ src/doodle/jump/equipo7/grafico/Fondo.java
@@ -2,15 +2,52 @@ package doodle.jump.equipo7.grafico;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class Fondo extends JFrame {
-    JLayeredPane juego = new JLayeredPane();
+    private JLayeredPane juego = new JLayeredPane();
+    private JLabel labelDino;
+    private ImageIcon imagenDinoDerecha;
+    private ImageIcon imagenDinoIzquierda;
+
     public Fondo() {
         super("DOODLE JUMP");
         setLayout(new BorderLayout());
-        ImageIcon icon = new ImageIcon(getClass().getResource("/images/Smily.png"));
-        Image iconImage = icon.getImage();
-        setIconImage(iconImage);
+
+        // Cargar imágenes del dino
+        imagenDinoDerecha = new ImageIcon(getClass().getResource("/images/SmilyD.png"));
+        imagenDinoIzquierda = new ImageIcon(getClass().getResource("/images/SmilyI.png"));
+
+        // Configurar la imagen inicial del dino
+        labelDino = new JLabel(imagenDinoDerecha);
+        labelDino.setBounds(50, 50, imagenDinoDerecha.getIconWidth(), imagenDinoDerecha.getIconHeight());
+        juego.add(labelDino, Integer.valueOf(1));
+
+        // Agregar controlador de eventos de teclado
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                // No es necesario para este caso
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int key = e.getKeyCode();
+                if (key == KeyEvent.VK_LEFT) {
+                    moverSmilyIzquierda();
+                } else if (key == KeyEvent.VK_RIGHT) {
+                    moverSmilyDerecha();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                // No es necesario para este caso
+            }
+        });
+
+        setFocusable(true); // Permitir que el JFrame reciba eventos de teclado
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 620);
         setLocationRelativeTo(null);
@@ -18,26 +55,35 @@ public class Fondo extends JFrame {
 
     public void mostrarFondoyPersonaje() {
         crearFondo();
-        personaje();
-        add(this.juego, BorderLayout.CENTER);
+        add(juego, BorderLayout.CENTER);
         setResizable(false);
         setVisible(true);
     }
 
-    public void crearFondo(){
-        this.juego.setPreferredSize(new Dimension(400, 620));
+    public void crearFondo() {
+        juego.setPreferredSize(new Dimension(400, 620));
 
         ImageIcon imagenFondo = new ImageIcon(getClass().getResource("/images/doodleFondo.png"));
-        JLabel label1 = new JLabel(imagenFondo);
-        label1.setBounds(0, 0, 400, 620);
-        this.juego.add(label1, Integer.valueOf(0));
-
+        JLabel labelFondo = new JLabel(imagenFondo);
+        labelFondo.setBounds(0, 0, 400, 620);
+        juego.add(labelFondo, Integer.valueOf(0));
     }
 
-    public void personaje(){
-        ImageIcon imagenDino = new ImageIcon(getClass().getResource("/images/Smily.png"));
-        JLabel label2 = new JLabel(imagenDino);
-        label2.setBounds(50, 50, imagenDino.getIconWidth(), imagenDino.getIconHeight());
-        this.juego.add(label2, Integer.valueOf(1));
+    public void moverSmilyIzquierda() {
+        labelDino.setIcon(imagenDinoIzquierda);
+        int x = labelDino.getX();
+        if (x > 0) {
+            x -= 10; // Ajusta la cantidad de píxeles que el dino se mueve a la izquierda
+            labelDino.setLocation(x, labelDino.getY());
+        }
+    }
+
+    public void moverSmilyDerecha() {
+        labelDino.setIcon(imagenDinoDerecha);
+        int x = labelDino.getX();
+        if (x < getWidth() - labelDino.getWidth()) {
+            x += 10; // Ajusta la cantidad de píxeles que el dino se mueve a la derecha
+            labelDino.setLocation(x, labelDino.getY());
+        }
     }
 }
